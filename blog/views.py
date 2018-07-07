56bb37f9ea76e1f0d5651a83ec075b4b01a6a34a
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-07-07 12:41:33 -0400

    Add category support to blog

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 import markdown2
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Category
+
+def category(request, category):
+    posts = Post.objects.filter(category=category)
+    categories = Category.objects.all()
+    return render(request, 'blog.html', {'posts': posts, 'categories': categories, 'select': True})
 
 def blog(request):
     posts = Post.objects.all()
-    return render(request, 'blog.html', {'posts': posts })
+    categories = Category.objects.all()
+    return render(request, 'blog.html', {'posts': posts, 'categories': categories, 'select': False})
 
 def post(request, id):
     post = Post.objects.get(id=id)
     post.body = markdown2.markdown(post.body)
-    return render(request, 'post.html', {'post': post })
+    return render(request, 'post.html', {'post': post})
--- kirubarajan/urls.py
+++ kirubarajan/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import blog, post
+from blog.views import blog, post, category
 from about.views import splash, projects, research
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('projects/', projects, name='projects'),
     path('research/', research, name='research'),
     path('post/<str:id>', post, name='post'),
+    path('blog/category/<int:category>', category, name='category'),
     path('blog/', blog, name='blog'),
     path('', splash, name='splash')
 ]
