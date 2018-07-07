0d74529bf1128b6e8d53c837c25fea68bc616263
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-07-07 15:08:01 -0400

    Add google static storage

--- kirubarajan/settings.py
+++ kirubarajan/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-STATIC_URL = STATIC_ROOT
+STATIC_URL = "https://storage.googleapis.com/kirubarajan-site.appspot.com/assets/"
 
 STATICFILES_DIRS = [
     os.path.join(SITE_ROOT, 'assets/'),
