02d95f69a8e383f1301360ef99d08bd7e8666196
Arun Kirubarajan <arun@Aruns-MacBook-Pro.local>
2018-07-26 17:19:27 -0400

    Add Dockerfile and update deployment

--- kirubarajan/settings.py
+++ kirubarajan/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-STATIC_URL = "https://storage.googleapis.com/kirubarajan-site.appspot.com/assets/"
+STATIC_URL = "https://kirubarajan.nyc3.digitaloceanspaces.com/assets/"
 
 STATICFILES_DIRS = [
     os.path.join(SITE_ROOT, 'assets/'),
