88a28a329aab75f51c9de7d0f3b4a9d0cafa938a
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-10-10 22:29:56 -0400

    Add static file deployment URL

--- kirubarajan/urls.py
+++ kirubarajan/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from blog.views import blog, post, category
 from about.views import splash, projects, research
@@ -11,4 +13,4 @@ urlpatterns = [
     path('blog/category/<int:category>', category, name='category'),
     path('blog/', blog, name='blog'),
     path('', splash, name='splash')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
