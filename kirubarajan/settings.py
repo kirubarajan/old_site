62e3692df211957a85e2e0dc812913f84705d30a
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-10-11 10:07:05 -0400

    Add debug check

--- kirubarajan/settings.py
+++ kirubarajan/settings.py
@@ -116,10 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-STATIC_URL = "https://kirubarajan.nyc3.digitaloceanspaces.com/assets/"
+if not DEBUG:
+    SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+    STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
+    STATIC_URL = "https://kirubarajan.nyc3.digitaloceanspaces.com/assets/"
 
-STATICFILES_DIRS = [
-    os.path.join(SITE_ROOT, 'assets/'),
-]
+    STATICFILES_DIRS = [
+        os.path.join(SITE_ROOT, 'assets/'),
+    ]
+else:
+    STATIC_URL = '/static/'
