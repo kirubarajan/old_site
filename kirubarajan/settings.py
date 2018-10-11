e846dbca0ea476935853ad48ae25204b8193be20
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-10-10 21:04:23 -0400

    Update blog

--- kirubarajan/settings.py
+++ kirubarajan/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'kirubarajan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'kirubarajan/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,10 +116,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-STATIC_URL = "https://kirubarajan.nyc3.digitaloceanspaces.com/assets/"
-
-STATICFILES_DIRS = [
-    os.path.join(SITE_ROOT, 'assets/'),
-]
+STATIC_URL = '/static/'
